989f35e97894246459f52aeed436ec2bc4ccc228
yubinshensyb <2493599153@qq.com>
2020-10-25 23:46:13 +0800

    增加新文件

--- src/main/java/com/atguigu/love.java
+++ src/main/java/com/atguigu/love.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class love {
     public static void main(String[] args) {
-        System.out.println("sss = " + 111);
+        System.out.println("sss = " + 1144444);
     }
 }
